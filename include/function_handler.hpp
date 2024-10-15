c132b1043b3e0e1abcbfdb6211135584265024dd
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-10-15 13:13:37 +0200

    callFunction method of handler + test with differnet nunber fo parameters

--- include/function_handler.hpp
+++ include/function_handler.hpp
@@ -18,7 +18,7 @@ private:
     std::vector<std::vector<double>> paramSpace; 
 
 public:
-    FunctionHandler(Args... args) 
+    FunctionHandler(InputFunction inputFunction, Args... args) : functionVariant(inputFunction) 
     {
         (paramSpace.push_back(args),...);
     }
@@ -29,5 +29,29 @@ public:
         return paramSpace;
     }
 
+
+    double callFunction(double x, double param1 = 0, double param2 = 0, double param3 = 0) 
+    {
+        std::vector<size_t> typeIndex = {0, 1};
+        for (size_t index: typeIndex)
+        {
+            if (index == 0) 
+            {
+                if (auto functionPtr = std::get_if<std::function<double(const double&)>>(&functionVariant)) 
+                {
+                    return (*functionPtr)(x);
+                }
+            } 
+            else if (index == 1)
+            {
+                if (auto functionPtr = std::get_if<std::function<double(const double&, const double&, const double&, const double&)>>(&functionVariant))
+                {
+                    return (*functionPtr)(x, param1, param2, param3);
+                }
+            }
+        }
+    }
+
+ 
 private:
 };
--- test/unit_tests/test_function_handler.cpp
+++ test/unit_tests/test_function_handler.cpp
@@ -4,18 +4,68 @@
 #include "function_handler.hpp"
 
 
+class TestClass
+{
+public:
+    double testMethod(const double& x)
+    {
+        return x;
+    }
+};
+
+
+double testFunction(const double& x)
+{
+    return x;
+}
+
+
+double testFunction3Params(const double& x, const double& param1, const double& param2, const double& param3)
+{
+    return param1 * (x*x) + param2 * x + param3;
+}
+
+
 BOOST_AUTO_TEST_SUITE( FunctionHandlerTestSuite )
+
 
 BOOST_AUTO_TEST_CASE( TestParamSpaceConstruction ) {
     std::vector<double> param1 = {2, 3, 5, 6, 7, 0};
     std::vector<double> param2 = {1, 25, 7, 3, 6, 2};
 
-    FunctionHandler<std::vector<double>, std::vector<double>> handler(param1, param2);
+    FunctionHandler<std::vector<double>, std::vector<double>> handler(testFunction, param1, param2);
 
     std::vector<std::vector<double>> paramSpace = handler.getParamSpace();
 
     BOOST_CHECK_EQUAL(paramSpace.size(), 2);
     BOOST_CHECK_EQUAL(paramSpace[0].size(), 6);
 }
+
+
+BOOST_AUTO_TEST_CASE( TestFunctionProcessing1Param ) {
+    std::vector<double> param1 = {2, 3, 5, 6, 7, 0};
+    std::vector<double> param2 = {1, 25, 7, 3, 6, 2};
+
+    FunctionHandler<std::vector<double>, std::vector<double>> handler(testFunction, param1, param2);
+
+    double testValue;
+    testValue = handler.callFunction(5);
+
+    BOOST_CHECK_EQUAL(testValue, 5.0);
+}
+
+
+BOOST_AUTO_TEST_CASE( TestFunctionProcessing3Param ) {
+    std::vector<double> param1 = {2, 3, 5, 6, 7, 0};
+    std::vector<double> param2 = {1, 25, 7, 3, 6, 2};
+
+    FunctionHandler<std::vector<double>, std::vector<double>> handler(testFunction3Params, param1, param2);
+
+    double testValue;
+    testValue = handler.callFunction(5, 2, 4, -6);
+
+    BOOST_CHECK_EQUAL(testValue, 64.0);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
