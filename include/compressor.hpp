5a9b254c116a035552dbf0b5d1fa6a1ea3c004d8
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-10-23 10:59:32 +0200

    Compressor class setup

--- include/compressor.hpp
+++ include/compressor.hpp
@@ -1,11 +1,46 @@
+#include "function_handler.hpp"
+#include "interval_divider.hpp"
+
+template<typename T>
 class Compressor
 {
 private:
-    
+    T handler;
+    std::vector<double> weights;
+    std::vector<double> nodes;
+    std::vector<std::vector<double>> values;
+    matrix<double> A;
+    matrix<double> U;
+
 
 public:
+    Compressor() 
+    {
+
+    }
+
+    ~Compressor() 
+    {
+
+    }
 
 private:
+    void constructA()
+    {
+
+    }
+
+
+    void decomposeIntoQR()
+    {
+
+    }
+
+
+    void scaleU()
+    {
+
+    }
 
 protected:
 };
---
+++ test/unit_tests/test_compressors.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE CompressorTestSuite
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include "compressor.hpp"
+
+BOOST_AUTO_TEST_SUITE( CompressorTestSuite )
+
+
+BOOST_AUTO_TEST_SUITE_END()
