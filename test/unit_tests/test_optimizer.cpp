6f43bdaf06bf4707062487fbbb3c5d0db3c30183
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-12-18 19:02:28 +0100

    set up optimizer class for the Gauss Newton procedure

---
+++ test/unit_tests/test_optimizer.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE OptimizerTestSuite
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include "ggq.hpp"
+
+BOOST_AUTO_TEST_SUITE( OptimizerTestSuite )
+
+BOOST_AUTO_TEST_CASE( TestOptimizerConstructor ) {
+
+} 
+
+BOOST_AUTE_TEST_SUITE_END()
